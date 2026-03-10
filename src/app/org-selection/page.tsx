c6ea864bb4ecae16494f93dfbd9a9e30c57b5c52
masterthuku <peterthuku706@gmail.com>
2026-03-11 02:53:15 +0300

    database

---
+++ src/app/org-selection/page.tsx
@@ -0,0 +1,22 @@
+import { OrganizationList } from "@clerk/nextjs";
+import React from "react";
+
+const OrgSelectionPage = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-background">
+      <OrganizationList
+        hidePersonal
+        afterCreateOrganizationUrl={"/"}
+        afterSelectOrganizationUrl={"/"}
+        appearance={{
+          elements: {
+            rootBox: "mx-auto",
+            card: "shadow-lg",
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default OrgSelectionPage;
