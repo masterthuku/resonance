483d9a89c48d3a29e06d0f29aad201968577e449
masterthuku <peterthuku706@gmail.com>
2026-03-14 18:29:19 +0300

    sign

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,14 @@
+import { SidebarTrigger } from "@/components/ui/sidebar"
+
+const DashboadPage = () => {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+        <SidebarTrigger/>
+        <h1 className="text-2xl font-semibold">
+            Dashboard
+        </h1>
+    </div>
+  )
+}
+
+export default DashboadPage
